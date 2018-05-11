a00121bccf14f1718d2bd9309309a35c4d91c07d
Rachel Whitty <rach.cww@gmail.com>
2018-05-11 18:31:25 +0100

    Added a test script

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello")
+from usbiss.spi import SPI
+
+spi = SPI(port)
+
+spi.mode = 1
+spi.max_speed_hz = 500000
+
+print(repr(spi._usbiss))
+
+# SPI transaction
+
+response = spi.xfer([0x00, 0x00])
